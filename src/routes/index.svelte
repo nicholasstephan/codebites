1b81c7bfceb54bcba8bc71d7cf6c3c6bcbc4129b
Nicholas Stephan <nicholas.stephan@gmail.com>
2022-03-26 13:36:34 -0700

    wip

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,43 @@
+<script context="module">
+
+  export async function load({_, fetch}) {
+    const url = `https://datadatadata.online/rest/codebites/bites`;
+    let response = await fetch(url);
+    return {
+      status: response.status,
+      props: {
+        bites: response.ok && (await response.json())
+      }
+    };
+  }
+
+</script>
+
+<script>
+
+  export let bites = [];
+
+  console.log(bites);
+
+</script>
+
+<header>
+  <h1>&lt;CODE/&gt;</h1>
+</header>
+
 <article>
-  <h1>&lt;CODE BITES/&gt;</h1>
+  {#each bites as bite}
+    <a href="/{bite.id}">{bite.title}</a>
+  {/each}
+
 </article>
 
 
 <style>
 
-  article {
+  header {
     box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-    padding-bottom: 20vh;
-
+    height: 600px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -19,6 +46,18 @@
   h1 {
     font-size: 80px;
     font-family: 'Roboto Mono', monospace;
+  }
+
+  article {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  article a {
+    display: block;
+    color: white;
+    text-decoration: none;
+    padding: 4px 0;
   }
   
 </style>
